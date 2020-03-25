132e6d9b80d563aac1947838c16c07d0f3287e80
poggonet01 <artiompogonet98@gmail.com>
2020-03-25 22:16:20 +0700

    Laboratorul5

--- InvalidCatalogException.java
+++
@@ -1,9 +0,0 @@
-package com.Laborator5.se.task1;
-
-public class InvalidCatalogException extends Exception {
-    public InvalidCatalogException (Exception ex){
-        super("Invalid catalog file : " , ex);
-    }
-
-
-}
